ad0190a5f45a73f509c2807c2cd8d90fec0954a9
Chetan Yewale <yewalechetan@gmail.com>
2021-07-16 20:47:14 +0530

    updated logic of pairWithTarget

--- 2. Pattern Two Pointers/pairWithTargetSum.py
+++ 2. Pattern Two Pointers/pairWithTargetSum.py
@@ -4,18 +4,17 @@ def findPair(sortedArray, targetSum):
     left = 0
     right = len(sortedArray) - 1
     pair = []
-    while left < len(sortedArray):       
-        while left < right:
-            currSum = sortedArray[left] + sortedArray[right]
-            if currSum == targetSum:
-                pair.append(sortedArray[left])
-                pair.append(sortedArray[right])
-                return pair
-            elif currSum > targetSum:
-                right = right - 1
-            elif currSum < targetSum:
-                break
-        left = left + 1
+    while left < right:
+        currSum = sortedArray[left] + sortedArray[right]
+        if currSum == targetSum:
+            pair.append(sortedArray[left])
+            pair.append(sortedArray[right])
+            return pair
+        elif currSum > targetSum:
+            right = right - 1
+        else:
+            left = left + 1
+
 
         
 def test_findPair_test1():
@@ -24,3 +23,7 @@ def test_findPair_test1():
 
 def test_findPair_test2():    
     assert findPair([2,3,5,9,11], 12) == [3,9]
+
+    
+def test_findPair_test3():    
+    assert findPair([2,3,5,10,11], 15) == [5,10]
