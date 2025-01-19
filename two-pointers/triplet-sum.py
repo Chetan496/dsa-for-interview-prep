7e5d2c62ebcc12bcb341b478f51904eb5769995f
Chetan496 <yewalechetan@gmail.com>
2025-01-19 20:39:33 +0530

    feat: working on triplet sum

---
+++ two-pointers/triplet-sum.py
@@ -0,0 +1,27 @@
+# Given an integer array nums, find all unique triplets in the array that give a sum of zero (0). The solution set must not contain duplicate triplets.
+# Write a function that returns a list of all unique triplets [nums[i], nums[j], nums[k]] such that:
+
+# i != j != k
+# 0 <= i, j, k < nums.length
+# The sum nums[i] + nums[j] + nums[k] == 0
+
+# Constraints:
+
+# 3 ≤ nums.length ≤ 3000
+# -105 ≤ nums[i] ≤ 105
+
+# Examples:
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    list = [-5,-2,3,4,6]      
